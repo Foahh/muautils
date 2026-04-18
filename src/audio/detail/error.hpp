56078267fa9175d06806d3676cd564df42077c03
Foahh <160589889+Foahh@users.noreply.github.com>
2026-04-19 04:41:44 +0800

    refactor(audio): extract av error helpers into detail/error.hpp

---
+++ src/audio/detail/error.hpp
@@ -0,0 +1,44 @@
+// src/audio/detail/error.hpp
+#pragma once
+
+extern "C" {
+#include <libavutil/error.h>
+}
+
+#include "lib.hpp"
+#include <fmt/format.h>
+#include <stdexcept>
+
+namespace av {
+
+template <typename... Args>
+void Assert(const int err, const fs::path &path, fmt::format_string<Args...> msg_fmt, Args &&...args) {
+    if (err < 0) {
+        char txt[1024];
+        av_strerror(err, txt, sizeof(txt));
+        auto msg = fmt::format(msg_fmt, std::forward<Args>(args)...);
+        auto fmsg = fmt::format("{} (ffmpeg: {})", msg, txt);
+        throw lib::FileError(path, fmsg);
+    }
+}
+
+template <typename... Args>
+void Assert(const int err, fmt::format_string<Args...> msg_fmt, Args &&...args) {
+    if (err < 0) {
+        char txt[1024];
+        av_strerror(err, txt, sizeof(txt));
+        auto msg = fmt::format(msg_fmt, std::forward<Args>(args)...);
+        auto fmsg = fmt::format("{} (ffmpeg: {})", msg, txt);
+        throw std::runtime_error(fmsg);
+    }
+}
+
+template <typename... Args>
+void Ensure(const bool cond, fmt::format_string<Args...> msg_fmt, Args &&...args) {
+    if (!cond) {
+        const auto msg = fmt::format(msg_fmt, std::forward<Args>(args)...);
+        throw std::runtime_error(msg);
+    }
+}
+
+} // namespace av
--- src/audio/utils.hpp
+++ src/audio/utils.hpp
@@ -13,6 +13,7 @@ extern "C" {
 #include <memory>
 
 #include "audio.hpp"
+#include "audio/detail/error.hpp"
 #include "audio/detail/raii.hpp"
 #include "lib.hpp"
 #include "utils.hpp"
@@ -26,36 +27,6 @@ using detail::AVFilterGraphPtr;
 using detail::AVPacketPtr;
 using detail::AVFramePtr;
 }
-
-namespace av {
-template <typename... Args>
-void Assert(const int err, const fs::path &path, fmt::format_string<Args...> msg_fmt, Args &&...args) {
-    if (err < 0) {
-        char txt[1024];
-        av_strerror(err, txt, sizeof(txt));
-        auto msg = fmt::format(msg_fmt, std::forward<Args>(args)...);
-        auto fmsg = fmt::format("{} (ffmpeg: {})", msg, txt);
-        throw lib::FileError(path, fmsg);
-    }
-}
-
-template <typename... Args> void Assert(const int err, fmt::format_string<Args...> msg_fmt, Args &&...args) {
-    if (err < 0) {
-        char txt[1024];
-        av_strerror(err, txt, sizeof(txt));
-        auto msg = fmt::format(msg_fmt, std::forward<Args>(args)...);
-        auto fmsg = fmt::format("{} (ffmpeg: {})", msg, txt);
-        throw std::runtime_error(fmsg);
-    }
-}
-
-template <typename... Args> void Ensure(const bool cond, fmt::format_string<Args...> msg_fmt, Args &&...args) {
-    if (!cond) {
-        const auto msg = fmt::format(msg_fmt, std::forward<Args>(args)...);
-        throw std::runtime_error(msg);
-    }
-}
-} // namespace av
 
 namespace Audio {
 
